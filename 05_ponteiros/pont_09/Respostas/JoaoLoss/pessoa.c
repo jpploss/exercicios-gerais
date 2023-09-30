e5e06a72da5ab14f4c935f1c50dd7ce576e3c05f
jpploss <joao.loss@edu.ufes.br>
2023-09-30 19:04:10 -0300

    Fiz pont11, mas não foi o pessoa do pont9

--- 05_ponteiros/pont_09/Respostas/JoaoLoss/pessoa.c
+++ 05_ponteiros/pont_09/Respostas/JoaoLoss/pessoa.c
@@ -3,7 +3,7 @@
 
 
 /**
- * @brief Cria uma nova pessoa e inicializa os ponteiros.
+ * @brief Cria uma nova pessoa com nome vazio (primeiro caractere igual a '\0') e inicializa os ponteiros para pai e mae com NULL.
  * 
  * @return Uma nova pessoa e sem pais.
  */
@@ -11,6 +11,7 @@ tPessoa CriaPessoa() {
     tPessoa pessoa;
     pessoa.mae = NULL;
     pessoa.pai = NULL;
+    pessoa.nome[0] = 0;
     return pessoa;
 }
 
@@ -28,30 +29,29 @@ void LePessoa(tPessoa *pessoa) {
 
 /**
  * @brief Imprime os dados de uma pessoa caso tenha pai e/ou mae associado(s).
+ * Dica: use a função VerificaSeTemPaisPessoa para verificar se a pessoa tem pai e/ou mae associado(s).
+ * Alem disso, imprimir o nome do irmao caso exista.
  * 
  * @param pessoa Ponteiro para a pessoa a ser impressa.
  */
 void ImprimePessoa(tPessoa *pessoa) {
-    if((*pessoa).pai == NULL && (*pessoa).mae == NULL) {
-        return;
+    
+    if(VerificaSeTemPaisPessoa(pessoa)) {
+        printf("NOME COMPLETO: %s\n", (*pessoa).nome);
+
+        if((*pessoa).pai == NULL) printf("PAI: NAO INFORMADO\n");
+        else printf("PAI: %s\n", (*((*pessoa).pai)).nome);
+
+        if((*pessoa).mae == NULL) printf("MAE: NAO INFORMADO\n");
+        else printf("MAE: %s\n", (*((*pessoa).mae)).nome);
+
+        printf("\n");
     }
-
-    printf("NOME COMPLETO: %s\n", (*pessoa).nome);
-
-    if((*pessoa).pai == NULL) printf("PAI: NAO INFORMADO\n");
-    else printf("PAI: %s\n", (*((*pessoa).pai)).nome);
-
-    if((*pessoa).mae == NULL) printf("MAE: NAO INFORMADO\n");
-    else printf("MAE: %s\n", (*((*pessoa).mae)).nome);
-
-    printf("\n");
-
     return;
 }
 
 /**
- * @brief Associa as famílias e grupos de pessoas.
- * Alem disso, e nessa funcao que sera lida a associacao entre as pessoas.
+ * @brief Le as associciacoes da entrada padrao e altera as pessoas de forma a representar as associacoes lidas
  * 
  * @param pessoas Ponteiro para a lista de pessoas a serem associadas.
  */
@@ -69,4 +69,17 @@ void AssociaFamiliasGruposPessoas(tPessoa *pessoas) {
         if(idPai != -1) pessoas[idFilho].pai = (pessoas + idPai);
         if(idMae != -1) pessoas[idFilho].mae = (pessoas + idMae);
     }
+}
+
+/**
+ * @brief Verifica se uma pessoa tem pai e/ou mae associado(s).
+ * Ou seja, verifica pelo menos um dos ponteiros pai e mae é diferente de NULL.
+ * 
+ * @param pessoa Ponteiro para a pessoa a ser verificada.
+ * 
+ * @return 1 se a pessoa tiver pai e/ou mae associado(s) e 0 caso contrário.
+*/
+int VerificaSeTemPaisPessoa(tPessoa *pessoa) {
+    if((*pessoa).mae != NULL || (*pessoa).pai != NULL) return 1;
+    return 0;
 }
