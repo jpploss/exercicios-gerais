e450a10a75afce0b638cb23d273a57113ef5426f
jpploss <joao.loss@edu.ufes.br>
2023-10-10 22:00:45 -0300

    terminando a TAD_pont_04

---
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_04/Respostas/JoaoLoss/aluno.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "aluno.h"
+
+/**
+ * @brief Cria um novo aluno vazio. Os atributos inteiros devem receber valor -1, e o ponteiro do nome deve apontar para NULL;
+ * 
+ * @return tAluno Retorna uma estrutura tAluno com os dados do aluno criado.
+ */
+tAluno* CriaAluno() {
+    tAluno* alunoPont = (tAluno*) malloc(sizeof(tAluno));
+    if(alunoPont == NULL) {
+        printf("Problema na alocação de memória para aluno.\n");
+        exit(1);
+    }
+    alunoPont->nome = NULL;
+    alunoPont->n1 = -1;
+    alunoPont->n2 = -1;
+    alunoPont->n3 = -1;
+    alunoPont->matricula = -1;
+    return alunoPont;
+}
+
+/**
+ * @brief Função para liberar toda a memória alocada dinamicamente para estrututas do tipo tAluno
+ * 
+ * @param aluno Ponteiro para estrutura do tipo tAluno a ser apagada.
+ */
+void ApagaAluno(tAluno *aluno) {
+    free(aluno);
+    return;
+}
+
+
+/**
+ * @brief Lê os dados de um aluno.
+ * 
+ * @param Ponteiro para aluno (alocado dinamicamente), cujos dados serão preenchidos nessa função
+ */
+void LeAluno(tAluno *aluno) {
+    scanf("%*[^A-Za-z]");
+
+    char nome[30];
+    scanf("%s", nome);
+    aluno->nome = nome;
+    scanf("%d %d %d %d", &(aluno->matricula), &(aluno->n1), &(aluno->n2), &(aluno->n3));
+    return;
+}
+
+/**
+ * @brief Compara as matrículas de dois alunos.
+ * 
+ * @param aluno1 Ponteiro para o primeiro aluno a ser comparado.
+ * @param aluno2 Ponteiro para o segundo aluno a ser comparado.
+ * @return int Retorna 1 se a matrícula do aluno1 é maior que a do aluno2, -1 se a matrícula do aluno1 é menor que a do aluno2 e 0 se as matrículas são iguais.
+ */
+int ComparaMatricula(tAluno* aluno1, tAluno* aluno2) {
+    if(aluno1->matricula > aluno2->matricula) return 1;
+    if(aluno1->matricula < aluno2->matricula) return -1;
+    if(aluno1->matricula == aluno2->matricula) return 0;
+}
+
+/**
+ * @brief Calcula a média das notas de um aluno.
+ * 
+ * @param aluno Ponteiro para aluno a ter a média calculada.
+ * @return int Retorna a média das notas do aluno.
+ */
+int CalculaMediaAluno(tAluno* aluno) {
+    int soma = aluno->n1 + aluno->n2 + aluno->n3;
+    return soma/3;
+}
+
+/**
+ * @brief Verifica se um aluno foi aprovado ou reprovado.
+ * 
+ * @param aluno Ponteiro para aluno a ser verificado.
+ * @return int Retorna 1 se o aluno foi aprovado e 0 se foi reprovado.
+ */
+int VerificaAprovacao(tAluno* aluno) {
+    int media = CalculaMediaAluno(aluno);
+    if(media >= 7) return 1;
+    return 0;
+}
+
+/**
+ * @brief Imprime os dados de um aluno.
+ * 
+ * @param aluno Ponteiro para aluno a ser impresso.
+ */
+void ImprimeAluno(tAluno* aluno) {
+    printf("%s", aluno->nome);
+}
---
+++ 06_alocacao_dinamica/02_TAD_dinamico/TAD_pont_04/Respostas/JoaoLoss/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "aluno.h"
+#include <stdlib.h>
+
+int main() {
+    int qtd;
+    scanf("%d", &qtd);
+
+    tAluno** alunos = (tAluno**) malloc(sizeof(tAluno*)*qtd);
+    if(alunos == NULL) {
+        printf("Problemas na alocação de memória para o vetor de ponteiros para alunos.\n");
+        exit(1);
+    }
+
+    for(int i = 0; i < qtd; i++) {
+        *(alunos + i) = CriaAluno();
+        LeAluno(*(alunos + i));
+    }
+
+    tAluno* aux;
+    for(int i = 0; i < qtd; i++) {
+        for(int j = i + 1; j < qtd; j++) {
+            if(ComparaMatricula(*(alunos + i), *(alunos + j)) == 1) {
+                aux = *(alunos + i);
+                *(alunos + i) = *(alunos + j);
+                *(alunos + j) = aux;
+            }
+        }
+    }
+
+    for(int i = 0; i < qtd; i++) {
+        if(VerificaAprovacao(*(alunos + i))) {
+            ImprimeAluno(*(alunos + i));
+            printf("\n");
+        }
+        ApagaAluno(*(alunos + i));
+    }
+
+    free(alunos);
+
+    return 0;
+}
